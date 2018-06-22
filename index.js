a48f7b4e495f114bcb6ff935eaa9b163174ce810
Kokila Elangovan <kokila.elangovan@siriuscom.com>
2018-06-22 11:16:49 +0530

    updated password

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 // Create chat bot
 var connector = new builder.ChatConnector({
  appId: '9a5cb2ed-2698-400e-a140-d1ac8ae13645',
- appPassword: 'wcwxzAJ94{}jeARKVT337|:'
+ appPassword: 'lkeK396[%+'
 });
 var bot = new builder.UniversalBot(connector);
 // If a Post request is made to /api/messages on port 3978 of our local server, then we pass it to the bot connector to handle
